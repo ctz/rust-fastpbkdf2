c17581d6d908e535f9aa102ac0f43cdf271e5b65
Demur Rumed <junkmute@hotmail.com>
2020-12-07 16:18:03 +0000

    Update dependencies
    libc: 0.1 to 0.2
    gcc: 0.3 to cc 1.0
    fastpbkdf2 submodule updated to latest commit
    Update version to 0.2.0

--- build.rs
+++ build.rs
@@ -1,21 +1,21 @@
-extern crate gcc;
+extern crate cc;
 
 #[cfg(not(windows))]
 fn main() {
-  gcc::Config::new()
-    .file("fastpbkdf2/fastpbkdf2.c")
-    .include("fastpbkdf2/")
-    .flag("-std=c99")
-    .opt_level(3)
-    .compile("libfastpbkdf2.a");
+    cc::Build::new()
+        .file("fastpbkdf2/fastpbkdf2.c")
+        .include("fastpbkdf2/")
+        .flag("-std=c99")
+        .opt_level(3)
+        .compile("libfastpbkdf2.a");
 }
 
 #[cfg(windows)]
 fn main() {
-  gcc::Config::new()
-    .file("fastpbkdf2\\fastpbkdf2.c")
-    .include("fastpbkdf2\\")
-    .include("fastpbkdf2\\openssl\\include")
-    .opt_level(3)
-    .compile("libfastpbkdf2.a");
+    cc::Build::new()
+        .file("fastpbkdf2\\fastpbkdf2.c")
+        .include("fastpbkdf2\\")
+        .include("fastpbkdf2\\openssl\\include")
+        .opt_level(3)
+        .compile("libfastpbkdf2.a");
 }
